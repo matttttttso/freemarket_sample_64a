eb5fbc0be8a4bbb025d966de03131b61b178ccbf
chiriakuta <57344370+chiriakuta@users.noreply.github.com>
2019-12-12 20:28:39 +0900

    コントローラーメソッド定義

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,2 +1,23 @@
 class SignupController < ApplicationController
+
+  def step1
+  end
+
+  def step2
+  end
+
+  def step3
+  end
+
+  def step4
+  end
+
+  def done
+  end
+
+  private
+
+  def user_params
+  end
+  
 end
