a9106926c9fc180430a7604005dbd7ffbf079c04
chiriakuta <57344370+chiriakuta@users.noreply.github.com>
2019-12-13 11:59:28 +0900

    マイグレーションファイル／アソシエーション編集

--- db/migrate/20191212071717_create_addresses.rb
+++ db/migrate/20191212071717_create_addresses.rb
@@ -1,11 +1,12 @@
 class CreateAddresses < ActiveRecord::Migration[5.2]
   def change
     create_table :addresses do |t|
-      t.string :zip_code,     null: false
-      t.string :prefecture,   null: false
-      t.string :city,         null: false
-      t.string :block,        null: false
-      t.string :building,     null: false
+      t.integer :zip_code,     null: false
+      t.integer :prefecture,   null: false
+      t.string :city,          null: false
+      t.string :block,         null: false
+      t.string :building,      null: false
+      t.integer :phone_number
       t.references :user,     foreign_key: true
 
       t.timestamps
