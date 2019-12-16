2479640007cbab2bd16a5945b373a17df60d42c2
chiriakuta <57344370+chiriakuta@users.noreply.github.com>
2019-12-16 21:44:07 +0900

    step毎のバリデーションチェック

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,27 +1,17 @@
 class SignupController < ApplicationController
 
+  before_action :step1_valid, only: :step2
+  before_action :step2_valid, only: :step3
+
   def step1
     @user = User.new
   end
 
   def step2
-    # step1で入力されたデータをsessionに保存する
-    session[:nickname] = user_params[:nickname]
-    session[:email] = user_params[:email]
-    session[:password] = user_params[:password]
-    session[:family_name_kanji] = user_params[:family_name_kanji]
-    session[:first_name_kanji] = user_params[:first_name_kanji]
-    session[:family_name_kana] = user_params[:family_name_kana]
-    session[:first_name_kana] = user_params[:first_name_kana]
-    session[:birthdate_year] = user_params[:birthdate_year]
-    session[:birthdate_month] = user_params[:birthdate_month]
-    session[:birthdate_day] = user_params[:birthdate_day]
     @user = User.new
   end
 
   def step3
-    # step2で入力されたデータをsessionに保存する
-    session[:user_phone_number] = user_params[:phone_number]
     @user = User.new
     @user.build_address
   end
@@ -47,7 +37,7 @@ class SignupController < ApplicationController
       session[:id] = @user.id
       redirect_to done_signup_index_path
     else
-      render '/signup/step1'
+      render '/signup/step3'
     end
   end
 
@@ -61,6 +51,51 @@ class SignupController < ApplicationController
   end
 
   private
+
+  def step1_valid
+    session[:nickname] = user_params[:nickname]
+    session[:email] = user_params[:email]
+    session[:password] = user_params[:password]
+    session[:family_name_kanji] = user_params[:family_name_kanji]
+    session[:first_name_kanji] = user_params[:first_name_kanji]
+    session[:family_name_kana] = user_params[:family_name_kana]
+    session[:first_name_kana] = user_params[:first_name_kana]
+    session[:birthdate_year] = user_params[:birthdate_year]
+    session[:birthdate_month] = user_params[:birthdate_month]
+    session[:birthdate_day] = user_params[:birthdate_day]
+    @user = User.new(
+      nickname: session[:nickname],
+      email: session[:email],
+      password: session[:password],
+      family_name_kanji: session[:family_name_kanji],
+      first_name_kanji: session[:first_name_kanji],
+      family_name_kana: session[:family_name_kana],
+      first_name_kana: session[:first_name_kana],
+      birthdate_year: session[:birthdate_year],
+      birthdate_month: session[:birthdate_month],
+      birthdate_day: session[:birthdate_day],
+      phone_number: "09012345678"
+    )
+    render '/signup/step1' unless @user.valid?
+  end
+
+  def step2_valid
+    session[:user_phone_number] = user_params[:phone_number]
+    @user = User.new(
+      nickname: session[:nickname],
+      email: session[:email],
+      password: session[:password],
+      family_name_kanji: session[:family_name_kanji],
+      first_name_kanji: session[:first_name_kanji],
+      family_name_kana: session[:family_name_kana],
+      first_name_kana: session[:first_name_kana],
+      birthdate_year: session[:birthdate_year],
+      birthdate_month: session[:birthdate_month],
+      birthdate_day: session[:birthdate_day],
+      phone_number: session[:phone_number]
+    )
+    render '/signup/step2' unless @user.valid?
+  end
 
   def user_params
     params.require(:user).permit(
