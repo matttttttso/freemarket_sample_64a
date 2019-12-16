cdcf02f371e70b5f7bfd51cfaacdf866de38c132
matttttttso <u2.pinetail@gmail.com>
2019-12-16 12:37:36 +0900

    レビュー用ユーザーコントローラの記述

--- app/controllers/review_users_controller.rb
+++ app/controllers/review_users_controller.rb
@@ -1,7 +1,37 @@
 class ReviewUsersController < ApplicationController
+  def login  
+  end
+
+  def signup
+  end
+
+  def basic_info
+  end
+
+  def phone_num
+  end
+
+  def phone_num2
+  end
+
+  def address
+  end
+
+  def creditcard
+  end
+
+  def completion
+  end
+
   def mypage
   end
 
   def profile
+  end
+
+  def identity
+  end
+
+  def logout
   end
 end
