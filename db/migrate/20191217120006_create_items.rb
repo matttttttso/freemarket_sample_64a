250f77d2c8a393b175d9d4c737755e05f33f64af
matttttttso <u2.pinetail@gmail.com>
2019-12-17 21:32:07 +0900

    Itemsテーブルの各カラムの記述(db/migrate/2...6_create_items.rb)

--- db/migrate/20191217120006_create_items.rb
+++ db/migrate/20191217120006_create_items.rb
@@ -1,6 +1,18 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
+      t.string :name,            null: false
+      t.text :explain,           null: false
+      t.string :category,        null: false
+      t.string :size,            null: false
+      t.string :brand,           null: false
+      t.string :condition,       null: false
+      t.string :postage,         null: false
+      t.string :shipping_method, null: false
+      t.string :region,          null: false
+      t.string :shipping_days,   null: false
+      t.integer :price,          null: false
+      t.references :user,        foreign_key: true
 
       t.timestamps
     end
