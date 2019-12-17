cd773a229e38c4df51a5ce19a8a713f0de4566e8
chiriakuta <57344370+chiriakuta@users.noreply.github.com>
2019-12-17 19:51:33 +0900

    正規表現等調整

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,22 +3,18 @@ class Address < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to :user
 
-  before_save { self.zip_code = zip_code.gsub(/-/,'') }
-  before_save { self.phone_number = phone_number.gsub(/-/,'') }
-
-  VALID_ZIP_REGEX = /\A\d{8}\z/i
-  VALID_KATAKANA_REGEX = /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/
-  VALID_PHONE_REGEX = /\A\d{10}$|^\d{11}\z/
+  VALID_ZIP_CODE = /\A\d{3}-\d{4}\z/i
+  VALID_KATAKANA_REGEX = /\A[\p{katakana}\p{blank}ー－]+\z/
+  VALID_PHONE_REGEX = /\A\d{10,11}\z/
 
   # step3バリデーション
-  validates :family_name_kanji, presence: true
-  validates :first_name_kanji,  presence: true
-  validates :family_name_kana,  presence: true, format: { with: VALID_KATAKANA_REGEX, allow_blank: true }
-  validates :first_name_kana,   presence: true, format: { with: VALID_KATAKANA_REGEX, allow_blank: true }
-  validates :zip_code,          presence: true, format: { with: VALID_ZIP_REGEX, allow_blank: true }
-  validates :prefecture_id,     presence: true
-  validates :city,              presence: true
-  validates :block,             presence: true
-  validates :building
-  validates :phone_number,      format: { with: VALID_PHONE_REGEX }
+  validates :family_name,          presence: true
+  validates :first_name,           presence: true
+  validates :family_name_katakana, presence: true, format: { with: VALID_KATAKANA_REGEX }
+  validates :first_name_katakana,  presence: true, format: { with: VALID_KATAKANA_REGEX }
+  validates :zip_code,             presence: true, format: { with: VALID_ZIP_CODE }
+  validates :prefecture_id,        presence: true
+  validates :city,                 presence: true
+  validates :block,                presence: true
+  validates :telephone_number,     format: { with: VALID_PHONE_REGEX }, allow_blank: true
 end
