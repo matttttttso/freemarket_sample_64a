a72fbf13db9790bc3d002d74dcab716b069684f4
chiriakuta <57344370+chiriakuta@users.noreply.github.com>
2019-12-17 19:52:36 +0900

    カラム名変更による修正

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -33,6 +33,8 @@ class SignupController < ApplicationController
       phone_number: session[:user_phone_number]
     )
     @user.build_address(user_params[:address_attributes])
+
+    binding.pry
     if @user.save
       session[:id] = @user.id
       redirect_to done_signup_index_path
@@ -53,6 +55,7 @@ class SignupController < ApplicationController
   private
 
   def step1_valid
+    # step1で入力された項目をsessionに一次保存
     session[:nickname] = user_params[:nickname]
     session[:email] = user_params[:email]
     session[:password] = user_params[:password]
@@ -63,6 +66,7 @@ class SignupController < ApplicationController
     session[:birthdate_year] = user_params[:birthdate_year]
     session[:birthdate_month] = user_params[:birthdate_month]
     session[:birthdate_day] = user_params[:birthdate_day]
+    # 仮のインスタンスを作成しバリデーションの確認
     @user = User.new(
       nickname: session[:nickname],
       email: session[:email],
@@ -74,13 +78,16 @@ class SignupController < ApplicationController
       birthdate_year: session[:birthdate_year],
       birthdate_month: session[:birthdate_month],
       birthdate_day: session[:birthdate_day],
+      # 未入力項目にはバリデーションに通る値を入れる
       phone_number: "09012345678"
     )
     render '/signup/step1' unless @user.valid?
   end
 
   def step2_valid
+    # step2で入力された項目をsessionに一次保存
     session[:user_phone_number] = user_params[:phone_number]
+    # 仮のインスタンスを作成しバリデーションの確認
     @user = User.new(
       nickname: session[:nickname],
       email: session[:email],
@@ -92,7 +99,7 @@ class SignupController < ApplicationController
       birthdate_year: session[:birthdate_year],
       birthdate_month: session[:birthdate_month],
       birthdate_day: session[:birthdate_day],
-      phone_number: session[:phone_number]
+      phone_number: session[:user_phone_number]
     )
     render '/signup/step2' unless @user.valid?
   end
@@ -112,16 +119,16 @@ class SignupController < ApplicationController
       :phone_number,
       address_attributes: [
         :id,
-        :family_name_kanji,
-        :first_name_kanji,
-        :family_name_kana,
-        :first_name_kana,
+        :family_name,
+        :first_name,
+        :family_name_katakana,
+        :first_name_katakana,
         :zip_code,
         :prefecture_id,
         :city,
         :block,
         :building,
-        :phone_number
+        :telephone_number
       ]
     )
   end
