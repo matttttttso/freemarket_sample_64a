638f3d19cdf28964c3f5cb4896003b0d7c5fe4b1
pontarou <tomokakuugokou@gmail.com>
2019-12-18 16:39:20 +0900

    SNS認証（omniauth処理）
    ①気になったのでメルカリのiconのリンク先のurlを正しく修正(_main_footer.html.haml)
    
    ②omniauth処理

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_12_071717) do
+ActiveRecord::Schema.define(version: 2019_12_18_040253) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "family_name_kanji", null: false
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 2019_12_12_071717) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uid"
+    t.string "provider"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
