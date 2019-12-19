ca35bef0be57bf9ba6a41b070efa65d7d2f5b365
matttttttso <u2.pinetail@gmail.com>
2019-12-19 17:26:59 +0900

    カラム名変更(itemテーブル内、name→item_name)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_19_044611) do
+ActiveRecord::Schema.define(version: 2019_12_19_050557) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -51,7 +51,7 @@ ActiveRecord::Schema.define(version: 2019_12_19_044611) do
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "item_name", null: false
     t.text "explain", null: false
     t.string "category", null: false
     t.string "size", null: false
