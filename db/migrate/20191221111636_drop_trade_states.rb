c58a3b53249487c8513f0511d5d69f0e5dfc8169
matttttttso <u2.pinetail@gmail.com>
2019-12-21 20:39:19 +0900

    マイグレーションファイル削除(DropTradeStates)

--- db/migrate/20191221111636_drop_trade_states.rb
+++
@@ -1,5 +0,0 @@
-class DropTradeStates < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :trade_states
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_21_111636) do
+ActiveRecord::Schema.define(version: 2019_12_21_111139) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
