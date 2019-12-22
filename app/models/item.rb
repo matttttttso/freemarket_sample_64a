1c63f32189b8756c60f74dfae904d221118a809f
matttttttso <u2.pinetail@gmail.com>
2019-12-22 11:24:25 +0900

    Itemsテーブルバリデーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
   has_many_attached :images
   belongs_to :user
+  validates :item_name,        presence: true, length: { maximum: 40 }
+  validates :explain,          presence: true
+  validates :category,         presence: true
+  validates :size,             presence: true
+  validates :condition,        presence: true
+  validates :postage,          presence: true
+  validates :shipping_method,  presence: true
+  validates :prefecture_id,    presence: true
+  validates :shipping_days,    presence: true
+  validates :price,            numericality: {greater_than_or_equal_to: 300}
+  validates :price,            numericality: {less_than_or_equal_to: 9999999}
 end
