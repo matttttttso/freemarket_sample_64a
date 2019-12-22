1ec41629bc5563e1d6ff2dc4e70df3c9b2bf9fd7
matttttttso <u2.pinetail@gmail.com>
2019-12-22 11:39:31 +0900

    商品画像のバリデーション追記(ActiveStorage用)

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   has_many_attached :images
   belongs_to :user
+
   validates :item_name,        presence: true, length: { maximum: 40 }
   validates :explain,          presence: true
   validates :category,         presence: true
@@ -12,4 +13,16 @@ class Item < ApplicationRecord
   validates :shipping_days,    presence: true
   validates :price,            numericality: {greater_than_or_equal_to: 300}
   validates :price,            numericality: {less_than_or_equal_to: 9999999}
+
+  validate :images_presence
+
+  def images_presence
+    if images.attached?
+      if !images.content_type.in?(%('image/jpeg image/png'))
+        errors.add(:images, 'にはjpegまたはpngファイルを添付してください')
+      end
+    else
+      errors.add(:images, '画像がありません')
+    end
+  end
 end
