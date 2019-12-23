7a369260f0fe8d2ebfbf463d261389d8b1c82b6b
matttttttso <u2.pinetail@gmail.com>
2019-12-23 19:45:58 +0900

    Itemsコントローラ修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,13 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
   def index
-    # 後で実装
-    # @items = Item.all.with_attached_images
+    # 仮設置
+    @items = Item.all.with_attached_images
   end
 
   def show
     # 後で実装
-    # @item = Item.find(params[:id])
   end
 
   def sell
@@ -14,7 +15,16 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    # 後で実装
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to root_path, notice: '商品を編集しました'
+      # 詳細ページを作ったときに詳細ページに行くようにする
+      # redirect_to item_path(@item), notice: '商品を編集しました'
+    else
+      render :edit
+    end
   end
 
   def create
@@ -28,21 +38,25 @@ class ItemsController < ApplicationController
 
   private
 
-  def item_params
-    params.require(:item).permit(
-      :item_name,
-      :explain,
-      :category,
-      :size,
-      :brand,
-      :condition,
-      :postage,
-      :shipping_method,
-      :prefecture_id,
-      :shipping_days,
-      :price,
-      :trade_state,
-      images: []
-    ).merge(user_id: current_user.id)
-  end
+    def set_item
+      @item = Item.find(params[:id])
+    end
+
+    def item_params
+      params.require(:item).permit(
+        :item_name,
+        :explain,
+        :category,
+        :size,
+        :brand,
+        :condition,
+        :postage,
+        :shipping_method,
+        :prefecture_id,
+        :shipping_days,
+        :price,
+        :trade_state,
+        images: []
+      ).merge(user_id: current_user.id)
+    end
 end
