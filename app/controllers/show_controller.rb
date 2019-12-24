4f24c0637421091663a07fa3a6b56e2530b75933
matttttttso <u2.pinetail@gmail.com>
2019-12-24 18:17:47 +0900

    Merge branch 'ToDeleteEverythingAboutReviewUse' of https://github.com/matttttttso/freemarket_sample_64a into ToDeleteEverythingAboutReviewUse

--- app/controllers/show_controller.rb
+++ app/controllers/show_controller.rb
@@ -1,4 +1,11 @@
 class ShowController < ApplicationController
   def index
   end
+
+  def detail_content
+    @item = Item.find(params[:id])
+    @user = User.find(params[:id])
+    @address = Address.find(params[:id])
+    @prefecture = Prefecture.find(id: @address.prefecture_id)
+  end
 end
