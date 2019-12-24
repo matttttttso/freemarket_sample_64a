5f5a72941ef9c60228841181578f445ec1f76c19
matttttttso <u2.pinetail@gmail.com>
2019-12-24 17:51:50 +0900

    ルーティングの記述修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,23 @@
 Rails.application.routes.draw do
+  # root
   root to: "show#index"
-  
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  root to: "show#index"
+  # devise
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   devise_scope :user do
     delete :sign_out, to: 'devise/sessions#destroy'
   end
 
+  # show_controller
+  resources :show do
+    collection do
+      get 'detail_content'
+      get 'creditcard-registration'
+    end
+  end
+
+  # signup_controller
   resources :signup, only: [:index, :create] do
     collection do
       get 'step1'
@@ -16,10 +25,20 @@ Rails.application.routes.draw do
       get 'step3'
       # get 'step4' クレジットカードの登録は購入機能作成の際に同時に行う
       get 'done'
+    end
+  end
+
+  # user_controller
+  resource :users, only: :index do
+    collection do
+      get 'mypage'
+      get 'profile'
+      get 'identity'
       get 'logout'
     end
   end
 
+  # card_controller
   resources :card, only: [:new, :show] do
     collection do
       get 'confirmation', to: 'card#confirmation'
@@ -29,43 +48,11 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :items, except: [:new, :destroy] do
+  # items_controller
+  resources :items do
     collection do
       get 'sell'
       get 'purchase'
     end
   end
-
-  resources :show do
-    collection do
-      get 'detail_content'
-      get 'creditcard-registration'
-    end
-  end
-
-  resource :review_users, :review_items, only: [:index, :show]
-  
-  resource :review_users do
-    collection do
-      get 'login'
-      get 'signup'
-      get 'basic_info'
-      get 'phone_num'
-      get 'phone_num2'
-      get 'address'
-      get 'creditcard'
-      get 'completion'
-      get 'mypage'
-      get 'profile'
-      get 'identity'
-      get 'logout'
-    end
-  end
-
-  resource :review_items do
-    collection do
-      get 'sell'
-    end
-  end
-  
 end
